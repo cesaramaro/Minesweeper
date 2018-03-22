53effeb2fec6f21d49fbb522b5f5eeb46fda1b37
Cesar Amaro <cesar.amaro@upr.edu>
2018-03-21 20:20:08 -0400

    Methods to verify if mine exists on given coordinates and generate mines

--- MineSweeper/src/Mines.java
+++ MineSweeper/src/Mines.java
@@ -1,12 +1,68 @@
+import java.awt.Point;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Mines {
-	
+
+    private ArrayList<Point> mineList = new ArrayList<Point>();
+    private final int MAX_MINES = 10;
+    
 	private Mines[] MinesCoordinates;
 	private Mines[] MinesCoordinates2;
 	
+	// Constructor
+    public Mines() {
+        generateMines();
+        System.out.println(mineList); // Test
+    }
+	
+    
+    
+    /**
+     * Generate mines and add them to a list
+     */
+    // TODO Prevent mines from repeating
+    private void generateMines() {
+        Random generator = new Random();
+        int minesAmount = 0;
+        
+        while (minesAmount < MAX_MINES) {
+            Point coords = new Point();
+            int x = generator.nextInt(9);
+            int y = generator.nextInt(9);
+            
+            coords.setLocation(x, y);
+            mineList.add(coords);
+            minesAmount++;
+        }
+    }
+	
+    /*
+     * Verifies if the received coordinates are a mine
+     * @param x coordinate
+     * @param y coordinate 
+     * @return boolean
+     */
+    public boolean isMine(int x, int y) {
+        for (Point coords : mineList) {
+            if ((coords.getX() == x) && (coords.getY() == y)) {
+                return true;
+            } else {
+                continue;
+            }
+        }
+        return false;
+    }
+
+    /*
+     * Returns the list of mines
+     */
+    public ArrayList<Point> getMines() {
+        return mineList;
+    }
+    
 	//Array to place the mines coordinates
-	public Mines[] getMinesCoordinates(){
+	public Mines[] getMinesCoordinates() {
 		return MinesCoordinates;
 	}
 	
@@ -21,18 +77,12 @@ public class Mines {
 			this.setX(a);
 			this.setY(b);	
 		}
-	//Create random numbers
-	private Mines MinesCoordinates(){
-		Random generator = new Random();
-		return new Mines(generator.nextInt(9), generator.nextInt(9));
-	}
 	
 //	//With the CreateCoordinates method fill the Mines arrays
 //	public void FillsMinesCoordinates(){
 //		for()
 //	}
-	
-	
+    
 	public int MinesNearbyCount(int x, int y) {
 		int count = 0;
 		return 0;
@@ -48,13 +98,14 @@ public class Mines {
 		return false;
 	}
 
-//Getters
+	//Getters
 	public int getX() {
 		return x;
 	}
 	public int getY() {
 		return y;
 	}
+	
 	//Setters
 	public void setX(int x) {
 			this.x = x;
